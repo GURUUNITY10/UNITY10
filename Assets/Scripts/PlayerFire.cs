4ae224e563a475e2b55353f0c17efea012597b57
AHMATDDA <98415816+AHMATDDA@users.noreply.github.com>
2022-02-06 18:30:56 +0900

    플레이어 오류수정

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerFire : MonoBehaviour
 {
@@ -16,6 +17,8 @@ public class PlayerFire : MonoBehaviour
     public int weaponPower = 5;
     // 애니메이터 변수
     Animator anim;
+    // 총 발사 효과 오브젝트 배열
+    public GameObject[] eff_Flash;
 
     // Start is called before the first frame update
     void Start()
@@ -39,10 +42,10 @@ public class PlayerFire : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             // 만일 이동 블랜드 트리 파라미터의 값이 0이라면, 공격 애니메이션을 실시한다.
-            //if (anim.GetFloat("MoveMotion") == 0)
-            //{
-              //  anim.SetTrigger("Attack");
-            //}
+            if (anim.GetFloat("MoveMotion") == 0)
+            {
+                anim.SetTrigger("Attack");
+            }
 
             // 레이를 생성한 후 발사될 위치와 진행 방향을 설정한다.
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
@@ -50,23 +53,40 @@ public class PlayerFire : MonoBehaviour
             RaycastHit hitInfo = new RaycastHit();
 
             // 레이를 발사한 후 만일 부딪힌 물체가 있으면 피격 이펙트를 표시한다.
-            // 만일 레이에 부딪힌 대상의 레이어가 ‘Enemy’라면 데미지 함수를 실행한다.
-            if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            if (Physics.Raycast(ray, out hitInfo))
             {
-                //<애너미 다운받은 후 실행>  EnemyFSM eFSM = hitInfo.transform.GetComponent<EnemyFSM>();
-                //<애너미 다운받은 후 실행>  eFSM.HitEnemy(weaponPower);
+                // 만일 레이에 부딪힌 대상의 레이어가 ‘Enemy’라면 데미지 함수를 실행한다.
+                if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                {
+                    EnemyFSM eFSM = hitInfo.transform.GetComponent<EnemyFSM>();
+                    eFSM.HitEnemy(weaponPower);
+                }
+                // 그렇지 않다면, 레이에 부딪힌 지점에 피격 이펙트를 플레이한다.
+                else
+                {
+                    // 피격 이펙트의 위치를 레이가 부딪힌 지점으로 이동시킨다.
+                    bulletEffect.transform.position = hitInfo.point;
+                    // 피격 이펙트의 forward 방향을 레이가 부딪힌 지점의 법선 벡터와 일치시킨다.
+                    bulletEffect.transform.forward = hitInfo.normal;
+                    // 피격 이펙트를 플레이한다.
+                    ps.Play();
+                }
             }
-            // 그렇지 않다면, 레이에 부딪힌 지점에 피격 이펙트를 플레이한다.
-            else
-            {
-                // 피격 이펙트의 위치를 레이가 부딪힌 지점으로 이동시킨다.
-                bulletEffect.transform.position = hitInfo.point;
-                // 피격 이펙트의 forward 방향을 레이가 부딪힌 지점의 법선 벡터와 일치시킨다.
-                bulletEffect.transform.forward = hitInfo.normal;
-                // 피격 이펙트를 플레이한다.
-                ps.Play();
-            }
+            // 총 이펙트를 실시한다.
+            StartCoroutine(ShootEffectOn(0.05f));
         }
+    }
+    // 총구 이펙트 코루틴 함수
+    IEnumerator ShootEffectOn(float duration)
+    {
+        // 랜덤하게 숫자를 뽑는다.
+        int num = Random.Range(0, eff_Flash.Length - 1);
+        // 이펙트 오브젝트 배열에서 뽑힌 숫자에 해당하는 이펙트 오브젝트를 활성화한다.
+        eff_Flash[num].SetActive(true);
+        // 지정한 시간만큼 기다린다.
+        yield return new WaitForSeconds(duration);
+        // 이펙트 오브젝트를 다시 비활성화한다.
+        eff_Flash[num].SetActive(false);
     }
 }
 
